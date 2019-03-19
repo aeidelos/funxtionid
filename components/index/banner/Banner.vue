f071beff6b5e2d6ac6863ff52c1551395a659ea3
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2019-03-19 23:37:17 +0700

    add template

---
+++ components/index/banner/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <v-parallax :src="hero" height="600" alt="funxtion image">
+      <v-layout column align-center justify-center class="white--text">
+        <img src="assets/vuetify.png" alt="Vuetify.js" height="200" />
+        <h1 class="white--text mb-2 display-1 text-xs-center">
+          Parallax Template
+        </h1>
+        <div class="subheading mb-3 text-xs-center">Powered by Vuetify</div>
+        <v-btn class="blue lighten-2 mt-5" dark large href="/pre-made-themes">
+          Get Started
+        </v-btn>
+      </v-layout>
+    </v-parallax>
+  </section>
+</template>
+
+<script>
+import Hero from '@/assets/img/hero.jpeg'
+export default {
+  name: 'IndexBanner',
+  data() {
+    return {
+      hero: Hero
+    }
+  }
+}
+</script>
+
+<style scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -58,8 +58,24 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2019</span>
+    <v-footer dark height="auto">
+      <v-card class="flex" flat tile>
+        <v-card-title class="teal">
+          <strong class="subheading">
+            Get connected with us on social networks!
+          </strong>
+
+          <v-spacer></v-spacer>
+
+          <v-btn v-for="icon in icons" :key="icon" class="mx-3" dark icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+
+        <v-card-actions class="grey darken-3 justify-center">
+          &copy;2018 — <strong>Vuetify</strong>
+        </v-card-actions>
+      </v-card>
     </v-footer>
   </v-app>
 </template>
@@ -86,7 +102,14 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'FunxtionID',
+      icons: [
+        'fab fa-facebook',
+        'fab fa-twitter',
+        'fab fa-google-plus',
+        'fab fa-linkedin',
+        'fab fa-instagram'
+      ]
     }
   }
 }
